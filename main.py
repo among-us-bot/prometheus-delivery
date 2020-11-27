ef28d6aa704b0b45c1d3acade22f3723300e8006
TAG-Epic <admin@itzepic.net>
2020-11-27 19:16:55 +0100

    Feat: Add new metric types

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ Created by Epic at 11/7/20
 """
 from flask import Flask, request
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from prometheus_client import make_wsgi_app, Gauge
+from prometheus_client import make_wsgi_app, Gauge, Counter
 from waitress import serve
 
-metrics = {}
 
+class ResettingMetric(Gauge):
+    pass
+
+
+metrics = {}
+analytic_types = {
+    "gauge": Gauge,
+    "counter": Counter,
+    "resetting": ResettingMetric
+}
 
 app = Flask(__name__)
 app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
@@ -15,12 +24,15 @@ app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
 })
 
 
-@app.route("/api/<string:name>/gauge", methods=["DELETE", "POST", "PATCH"])
-def update_stats(name):
+@app.route("/api/<string:name>/<string:analytic_type>", methods=["DELETE", "POST", "PATCH"])
+def update_stats(name, analytic_type):
     metric = metrics.get(name, None)
     value = int(request.args.get("value", 1))
     if metric is None:
-        metric = Gauge(name, name)
+        metric_type = analytic_types.get(analytic_type)
+        if metric_type is None:
+            return None, 404
+        metric = metric_type(name, name)
         metrics[name] = metric
     if request.method == "DELETE":
         metric.dec(value)
